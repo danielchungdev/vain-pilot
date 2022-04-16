4967eb50a8838944038a506489c57ae0f30565c5
= <pikachungg@gmail.com>
2022-04-16 19:10:37 -0400

    yes

---
+++ init-database/README.md
@@ -0,0 +1,27 @@
+# INIT-DATABASE
+## About
+INIT-DATABASE is a script that allows you to manage the VAIN database.
+
+Presents you with several options: 
+- Do everything
+- Drop database
+- Create database
+- Recreate database
+- Read file
+- Quit
+
+```
+Do everything: 
+```
+
+
+Requirements: 
+---
+- Node.js (v16.14.2)
+- PostgreSQL 14
+- **Makes sure a database called vain exists**
+
+#### How to run:
+1. run `npm install`
+2. run `npm start`
+
--- init-database/database/classes/Book.js
+++ init-database/database/classes/Book.js
@@ -1,3 +1,18 @@
+/**
+______             _      _     
+| ___ \           | |    (_)    
+| |_/ / ___   ___ | | __  _ ___ 
+| ___ \/ _ \ / _ \| |/ / | / __|
+| |_/ / (_) | (_) |   < _| \__ \
+\____/ \___/ \___/|_|\_(_) |___/
+                        _/ |    
+                       |__/     
+ */
+
+/**
+ * @description Class to represent a Book.
+ * @note This is only for the TSV file.
+ */
 class Book{
     constructor(type, authorship, subject, author, title, publisher, year, descriptor, namedPersons, notes, located ){
         this.type = type
