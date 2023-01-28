5408c7585bc2df926d4f9fbb595ab00c745fa401
Daniel Chung <pikachungg@gmail.com>
2023-01-28 05:47:26 -0500

    finished filter need to add year and words

--- vain-client/components/Filter.js
+++ vain-client/components/Filter.js
@@ -56,11 +56,11 @@ const Filter = ({handleAppliedFilters, handleResetFilters}) => {
         <div className="w-3/12">
             <button className="w-full h-16 bg-white border border-slate-300 rounded-md text-gray-700 font-semibold drop-shadow-md hover:bg-slate-100 transition duration-200 ease-in-out" onClick={resetFilters}>Reset Filter</button>
 			<div className="w-full bg-white border-slate-300 border rounded-md drop-shadow-md p-5 mt-5">
-				<h1 className="font-bold text-xl mb-5">Subjects</h1>
+				<h1 className="font-bold text-xl ">Subjects</h1>
 				{
 					subjects.map((subject, index) => (<p key={index}><input type="checkbox" onChange={handleSubjectChange} checked={subjectsChecked.includes(subject.subjectdescription)} value={subject.subjectdescription} className="mr-2"/>{subject.subjectdescription}</p>))
 				}
-				<h1 className="font-bold text-xl my-5">Types</h1>
+				<h1 className="font-bold text-xl mt-5">Types</h1>
 				{
 					types.map((type, index) => (<p key={index}><input type="checkbox" onChange={handleTypeChange} checked={typesChecked.includes(type.typedescription)} value={type.typedescription} className="mr-2"/>{type.typedescription}</p>))
 				}
